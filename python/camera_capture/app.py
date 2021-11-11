ef59e5ea66c6f180d281d646f3b989bf0ae88171
Jan Trebschuh <jan.trebschuh@accenture.com>
2021-11-11 22:05:57 +0100

    align path

--- python/camera_capture/app.py
+++ python/camera_capture/app.py
@@ -23,18 +23,22 @@ try:
     # Create a unique name for the container
     container_name = os.getenv('AZURE_STORAGE_CONTAINER_NAME')
 
-    camera = PiCamera()
-
     # Create a local directory to hold blob data
-    os.mkdir("./data")
-    upload_file_path = F"./data/${str(uuid.uuid4())}.jpg"
+    local_path = "./data"
+    os.mkdir(local_path)
+
+    # Create a file in the local data directory to upload and download
+    local_file_name = str(uuid.uuid4()) + ".jpg"
+    upload_file_path = os.path.join(local_path, local_file_name)
+
+    camera = PiCamera()
 
     camera.capture(upload_file_path)
 
     # Create a blob client using the local file name as the name for the blob
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=upload_file_path)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
 
-    print("\nUploading to Azure Storage as blob:\n\t" + upload_file_path)
+    print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
 
     # Upload the created file
     with open(upload_file_path, "rb") as data:
