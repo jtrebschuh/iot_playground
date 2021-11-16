aedd4715afc236a1643aa53cd6e9078e6f39754d
Jan Trebschuh <jan.trebschuh@gmail.com>
2021-11-16 21:52:26 +0100

    rework async iot

--- python/camera_capture/app.py
+++ python/camera_capture/app.py
@@ -8,17 +8,15 @@ logging.basicConfig(level=logging.INFO)
 
 gpiodevices.setup()
 
-gpiodevices.setLedGreen(True)
-time.sleep(0.5)
-gpiodevices.setLedYellow(True)
-time.sleep(0.5)
-
-gpiodevices.setLedYellow(False)
-gpiodevices.setLedGreen(False)
-
 iot_thread = threading.Thread(target=iot_camera.start_azure_iot())
 
 iot_thread.start()
+
+gpiodevices.setLedGreen(True)
+time.sleep(1)
+
+gpiodevices.setLedYellow(False)
+gpiodevices.setLedGreen(False)
 
 while True:
     gpiodevices.setLedGreen(gpiodevices.getButtonState())
