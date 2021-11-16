8af39dd39ff0e73639135601e19188b0053df07d
jtrebschuh <jan.trebschuh@gmail.com>
2021-11-16 22:30:06 +0100

    run_until_complete

--- python/camera_capture/iot_camera.py
+++ python/camera_capture/iot_camera.py
@@ -29,6 +29,73 @@ class IoT_Camera_Thread(threading.Thread):
 
         loop.run_until_complete(self.main())
         loop.close()
+        
+    # define behavior for handling methods
+    async def method1_listener(self):
+        while True:
+            method_request = await self.device_client.receive_method_request(
+                "p"
+            )  # Wait for method1 calls
+            print("executed method photo")
+
+
+            # Retrieve the connection string for use with the application. The storage
+            # connection string is stored in an environment variable on the machine
+            # running the application called AZURE_STORAGE_CONNECTION_STRING. If the environment variable is
+            # created after the application is launched in a console or with Visual Studio,
+            # the shell or application needs to be closed and reloaded to take the
+            # environment variable into account.
+            connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+
+            # Create the BlobServiceClient object which will be used to create a container client
+            blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+            # Create a unique name for the container
+            container_name = os.getenv('AZURE_STORAGE_CONTAINER_NAME')
+
+            # Create a local directory to hold blob data
+            local_path = "./data"
+            if not os.path.exists(local_path):
+                os.mkdir(local_path)
+
+            # Create a file in the local data directory to upload and download
+            local_file_name = str(uuid.uuid4()) + ".jpg"
+            upload_file_path = os.path.join(local_path, local_file_name)
+
+            camera = PiCamera()
+
+            camera.capture(upload_file_path)
+
+            # Create a blob client using the local file name as the name for the blob
+            blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+
+            print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
+
+            # Upload the created file
+            with open(upload_file_path, "rb") as data:
+                blob_client.upload_blob(data)
+            print("\nUploaded")
+            payload = {"result": True, "data": "execute successfully", "image": local_file_name}  # set response payload
+            status = 200  # set return status code
+
+            method_response = MethodResponse.create_from_method_request(
+                method_request, status, payload
+            )
+            
+            await self.device_client.send_method_response(method_response)  # send response
+
+    async def generic_method_listener(self):
+        while True:
+            method_request = (
+                await self.device_client.receive_method_request()
+            )  # Wait for unknown method calls
+            payload = {"result": False, "data": "unknown method"}  # set response payload
+            status = 400  # set return status code
+            print("executed unknown method: " + method_request.name)
+            method_response = MethodResponse.create_from_method_request(
+                method_request, status, payload
+            )
+            await self.device_client.send_method_response(method_response)  # send response
  
     async def main(self):
         
@@ -40,74 +107,7 @@ class IoT_Camera_Thread(threading.Thread):
         self.device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
 
         # connect the client.
-        await device_client.connect()
-
-        # define behavior for handling methods
-        async def method1_listener(device_client):
-            while True:
-                method_request = await device_client.receive_method_request(
-                    "p"
-                )  # Wait for method1 calls
-                print("executed method photo")
-
-
-                # Retrieve the connection string for use with the application. The storage
-                # connection string is stored in an environment variable on the machine
-                # running the application called AZURE_STORAGE_CONNECTION_STRING. If the environment variable is
-                # created after the application is launched in a console or with Visual Studio,
-                # the shell or application needs to be closed and reloaded to take the
-                # environment variable into account.
-                connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
-
-                # Create the BlobServiceClient object which will be used to create a container client
-                blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-
-                # Create a unique name for the container
-                container_name = os.getenv('AZURE_STORAGE_CONTAINER_NAME')
-
-                # Create a local directory to hold blob data
-                local_path = "./data"
-                if not os.path.exists(local_path):
-                    os.mkdir(local_path)
-
-                # Create a file in the local data directory to upload and download
-                local_file_name = str(uuid.uuid4()) + ".jpg"
-                upload_file_path = os.path.join(local_path, local_file_name)
-
-                camera = PiCamera()
-
-                camera.capture(upload_file_path)
-
-                # Create a blob client using the local file name as the name for the blob
-                blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
-
-                print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
-
-                # Upload the created file
-                with open(upload_file_path, "rb") as data:
-                    blob_client.upload_blob(data)
-                print("\nUploaded")
-                payload = {"result": True, "data": "execute successfully", "image": local_file_name}  # set response payload
-                status = 200  # set return status code
-
-                method_response = MethodResponse.create_from_method_request(
-                    method_request, status, payload
-                )
-                
-                await device_client.send_method_response(method_response)  # send response
-
-        async def generic_method_listener(device_client):
-            while True:
-                method_request = (
-                    await device_client.receive_method_request()
-                )  # Wait for unknown method calls
-                payload = {"result": False, "data": "unknown method"}  # set response payload
-                status = 400  # set return status code
-                print("executed unknown method: " + method_request.name)
-                method_response = MethodResponse.create_from_method_request(
-                    method_request, status, payload
-                )
-                await device_client.send_method_response(method_response)  # send response
+        await self.device_client.connect()
 
         # define behavior for halting the application
         # def stdin_listener():
@@ -119,8 +119,8 @@ class IoT_Camera_Thread(threading.Thread):
 
         # Schedule tasks for Method Listener
         listeners = asyncio.gather(
-            method1_listener(self.device_client),
-            generic_method_listener(self.device_client),
+            self.method1_listener(),
+            self.generic_method_listener(),
         )
 
         # Run the stdin listener in the event loop
