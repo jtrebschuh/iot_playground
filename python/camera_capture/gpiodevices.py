00b4281b3df05bf003a20ebccdf7a73bfb619fff
Jan Trebschuh <jan.trebschuh@gmail.com>
2021-11-16 19:39:19 +0100

    GPIO setup

--- python/camera_capture/gpiodevices.py
+++ python/camera_capture/gpiodevices.py
@@ -4,6 +4,11 @@ ledGreenPin = 11
 ledYellowPin = 32
 buttonPin = 12
 
+def setup():
+    GPIO.setmode(GPIO.BOARD)      # use PHYSICAL GPIO Numbering
+    GPIO.setup(ledGreenPin, GPIO.OUT)   # set ledPin to OUTPUT mode
+    GPIO.setup(ledYellowPin, GPIO.OUT)   # set ledPin to OUTPUT mode
+    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # set buttonPin to PULL UP INPUT mode
 
 def setLedGreen(state):
     if state:
